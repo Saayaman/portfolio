14cbcda71b7968d18b779df0b65ca17059301321
Saayaman <ayakosayama1987@gmail.com>
2017-01-04 14:19:37 -0800

    no message

--- app/index.php
+++ app/index.php
@@ -1,11 +1,42 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <?php include('parts/head.php'); ?>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <title>Super Saayaman | Portfolio of Ayakko Sayama</title>
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="css/style.css">
 </head>
   <body class="homePage">
     <header>
-        <?php include('parts/header.php'); ?>
+    <nav class="navbar">
+      <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand logo" href="index.php">SAAYA<br><p class="lato">ayako sayama</p></a>
+          </div>
+
+          <!--another nav button-->
+
+        <div class="lato collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+              <li><a href="index.php">HOME</a></li>
+              <li><a href="about.php">ABOUT ME</a></li>
+              <li><a href="works.php">WORKS</a></li>
+              <li><a href="contact.php">CONTACT</a></li>
+            </ul>
+        </div>
+    </div>
+    </nav>
+
     </header>
     <div id="main">
       <section class="section section-home">
